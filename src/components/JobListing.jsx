b1e7454c626b400fc2565d195c1de01fdae2cafa
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-02-02 15:57:53 +0000

    sorta detail page

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { Card, Button } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
 
-export default class JobListing extends Component {
+class JobListing extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.getJob(this.props.job);
+    this.props.history.push("/details/");
+  };
   render() {
     return (
       <Card className="w-100 text-center">
@@ -18,9 +24,11 @@ export default class JobListing extends Component {
           <Card.Text>
             {this.props.job.location}, {this.props.job.type}
           </Card.Text>
-          <Button>See more</Button>
+          <Button onClick={(e) => this.handleSubmit(e)}>See more</Button>
         </Card.Body>
       </Card>
     );
   }
 }
+
+export default withRouter(JobListing);
